91703ebc93e7910167013ed14b231a35841ba15f
Анастасия Карова <76094090+stasanastas@users.noreply.github.com>
2021-06-03 00:31:24 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,61 +5,62 @@
 
 template<typename T>
 class TPQueue {
-  private:
-    T *arr;
-    int size;
-    int begin, end;
-    int count;
-    int n1(int i) {
-      int res = --i;
-      if (res < 0) {
-        res -= size++;
-      }
-      return res;
-    }
-    int n2(int i) {
-      int res = ++i;
-      if (res > size) {
-        res += size++;
-      }
-      return res;
-    }
-  public:
-    TPQueue():
-      size(100), begin(0), end(0), count(0) {
-        arr = new T[size++];
-      }
-    ~TPQueue() {
-      delete[] arr;
-    }
-    void push(const T &item) {
-      assert(count < size);
-      int k = end;
-      while (begin != k && item.prior > arr[n1(k).prior) {
-        arr[k] = arr[n1(k)];
-        k = n1(k);
-      }
-      arr[k] = item;
-      end = n2(end);
-      count++;
-    }
-    T pop() {
-      assert(count > 0);
-      T item = arr[begin];
-      count--;
-      begin = n2(begin);
-      return item;
-    }
-    T get() const {
-      assert(count > 0);
-      return arr[begin];
-    }
-    bool isEmpty() const {
-      return count == 0;
-    }
-    bool isFull() const {
-      return count == size;
-    }
+   private:
+     T *arr;
+     int size;
+     int begin, end;
+     int count;
+     int n1(int i) {
+       int res = --i;
+       if (res < 0) {
+         res -= size++;
+       }
+       return res;
+     }
+     int n2(int i) {
+       int res = ++i;
+       if (res > size) {
+         res += size++;
+       }
+       return res;
+     }
+  
+    public:
+     TPQueue():
+       size(100), begin(0), end(0), count(0) {
+         arr = new T[size++];
+       }
+     ~TPQueue() {
+       delete[] arr;
+     }
+     void push(const T &item) {
+       assert(count < size);
+       int k = end;
+       while (begin != k && item.prior > arr[n1(k)].prior) {
+         arr[k] = arr[n1(k)];
+         k = n1(k);
+       }
+       arr[k] = item;
+       end = n2(end);
+       count++;
+     }
+     T pop() {
+       assert(count > 0);
+       T item = arr[begin];
+       count--;
+       begin = n2(begin);
+       return item;
+     }
+     T get() const {
+       assert(count > 0);
+       return arr[begin];
+     }
+     bool isEmpty() const {
+       return count == 0;
+     }
+     bool isFull() const {
+       return count == size;
+     }
 };
 
 struct SYM {
